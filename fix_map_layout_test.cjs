2f18c9d3b5f781d4304d6b34fe987343911fa374
sea-deep <seadeepie@gmail.com>
2026-09-01 19:23:15 +0530

    Cleanup test scripts

--- fix_map_layout_test.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('test/components/map/map-layout.test.ts', 'utf8');
-
-code = code.replace(/assignFeatureLanes\(([^,]+),\s*100\)/g, 'assignFeatureLanes($1)');
-
-fs.writeFileSync('test/components/map/map-layout.test.ts', code);
--- fix_seqline3.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/sequence/SequenceLine.tsx', 'utf8');
-
-code = code.replace(/  onTextMouseMove:\n/, '');
-
-fs.writeFileSync('src/components/sequence/SequenceLine.tsx', code);
